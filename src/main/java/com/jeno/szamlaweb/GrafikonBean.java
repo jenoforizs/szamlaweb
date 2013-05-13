eeeb001acbc38ee81192e08276940f08a3138153
Fórizs Jenő <jeno.forizs@vanio.hu>
2013-05-13 11:13:28 +0200

    fejlesztés

--- src/main/java/com/jeno/szamlaweb/GrafikonBean.java
+++ src/main/java/com/jeno/szamlaweb/GrafikonBean.java
@@ -13,8 +13,13 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Resource;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import org.primefaces.component.chart.bar.BarChart;
+import org.primefaces.event.ItemSelectEvent;
 import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.LineChartSeries;
 import org.springframework.stereotype.Component;
 
@@ -25,21 +30,26 @@ import org.springframework.stereotype.Component;
 @Component
 @ManagedBean(name = "GrafikonBean")
 public class GrafikonBean {
-
-    private CartesianChartModel adatModel;
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
     @Resource
     ForgalomService forgalomService;
     @Resource
     SzamlaService szamlaService;
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd");
+    
+    CartesianChartModel adatModelLine;
+    CartesianChartModel adatModelBar;
     double min = 0;
     double max = 0;
 
-    public String adatOsszeallitas() {
-        adatModel = new CartesianChartModel();
-
-        LineChartSeries cashflow = new LineChartSeries();
-        cashflow.setLabel("Cashflow");
+    public String adatOsszeallitasBeKi() {
+        min = 0;
+        
+        adatModelLine = new CartesianChartModel();
+        adatModelBar = new CartesianChartModel();
+        
+        ChartSeries bevetelekChart = new ChartSeries("Bevételek");
+        ChartSeries kiadasokChart = new ChartSeries("Kiadások");
 
         List all = new ArrayList();
 
@@ -50,9 +60,73 @@ public class GrafikonBean {
 
         Collections.sort(all, new DatumComparator());
 
-        for (Object o : all) {
-            System.out.println("o:" + o);
+        double napiBevetel = 0;
+        double napiKiadas = 0;
+
+        String nap = null;
+        String aktualisNap = null;
+
+        for (Object object : all) {
+            if (object instanceof Szamla) {
+                Szamla sz = (Szamla) object;
+                if (sz.getFajta() == Szamla.Fajta.BEVETELI) {
+                    napiBevetel += sz.getBruttoErtek();
+                } else if (sz.getFajta() == Szamla.Fajta.KIADASI) {
+                    napiKiadas += sz.getBruttoErtek();
+                } // if - else if
+                nap = sdf.format(sz.getFizetesIdopontja());
+            } // if
+
+            if (object instanceof Forgalom) {
+                Forgalom f = (Forgalom) object;
+                if (f.getFajta() == Forgalom.Fajta.BEJOVO) {
+                    napiBevetel += f.getOsszeg();
+                } else if (f.getFajta() == Forgalom.Fajta.KIMENO) {
+                    napiKiadas += f.getOsszeg();
+                } // if - else if
+                nap = sdf.format(f.getIdopont());
+            } // if
+
+            if (napiBevetel > max) {
+                max = napiBevetel;
+            } else if (napiKiadas * -1 < min) {
+                min = napiKiadas * -1;
+            } // if - else if
+
+            if (!nap.equals(aktualisNap)) {
+                if (napiBevetel != 0) {
+                    bevetelekChart.set(aktualisNap, napiBevetel);
+                } // if
+
+                if (napiKiadas != 0) {
+                    kiadasokChart.set(aktualisNap, napiKiadas);
+                } // if
+
+                aktualisNap = nap;
+                napiBevetel = 0;
+                napiKiadas = 0;
+            } // if
         } // for
+
+        adatModelBar.addSeries(bevetelekChart);
+        adatModelBar.addSeries(kiadasokChart);
+        
+        return "/pages/grafikonBeKi.jsf";
+    }
+
+    public String adatOsszeallitasCashflow() {
+        adatModelLine = new CartesianChartModel();
+        
+        LineChartSeries cashflow = new LineChartSeries("Cashflow");
+
+        List all = new ArrayList();
+
+        all.addAll(szamlaService.getOsszesFizetettSzamla(Szamla.Fajta.BEVETELI));
+        all.addAll(forgalomService.getOsszesForgalom(Forgalom.Fajta.BEJOVO));
+        all.addAll(szamlaService.getOsszesSzamla(Szamla.Fajta.KIADASI));
+        all.addAll(forgalomService.getOsszesForgalom(Forgalom.Fajta.KIMENO));
+
+        Collections.sort(all, new DatumComparator());
 
         double sum = 0;
         String nap = null;
@@ -76,24 +150,42 @@ public class GrafikonBean {
                 } // if - else if
                 nap = sdf.format(f.getIdopont());
             } // if
-            cashflow.set(nap, sum);
+
             if (sum > max) {
                 max = sum;
             } else if (sum < min) {
                 min = sum;
             } // if - else if
+
+            cashflow.set(nap, sum);
         } // for
 
-        adatModel.addSeries(cashflow);
-        return "/pages/grafikon.jsf";
+        adatModelLine.addSeries(cashflow);
+
+        return "/pages/grafikonCashflow.jsf";
     }
 
-    public CartesianChartModel getAdatModel() {
-        return adatModel;
+    public void kivalasztas(ItemSelectEvent event) {  
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",  
+                        "Item Index: " + event.getItemIndex() + ", Series Index:" + event.getSeriesIndex());  
+  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    } // kivalasztas
+    
+    public CartesianChartModel getAdatModelBar() {
+        return adatModelBar;
     }
 
-    public void setAdatModel(CartesianChartModel adatModel) {
-        this.adatModel = adatModel;
+    public void setAdatModelBar(CartesianChartModel adatModelBar) {
+        this.adatModelBar = adatModelBar;
+    }
+
+    public CartesianChartModel getAdatModelLine() {
+        return adatModelLine;
+    }
+
+    public void setAdatModelLine(CartesianChartModel adatModelLine) {
+        this.adatModelLine = adatModelLine;
     }
 
     public double getMin() {
